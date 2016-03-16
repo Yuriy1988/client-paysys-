eabd001bf428af6708572c85ea085071bdae33b0
ancoopa <ancoopa@gmail.com>
2016-03-16 09:35:25 +0200

    Invoice API update: 'invoice_get_info' API added.

--- app/handlers/invoice.py
+++ app/handlers/invoice.py
@@ -23,7 +23,7 @@ def invoice_create():
     {"order_id": "order_id_1", "store_id": "dss9-asdf-sasf-fsaa", "currency": "USD", "items": [{"item_id": "item_id_1",
     "quantity": 3, "unit_price": 23.5}, {"item_id": "item_id_2", "quantity": 1, "unit_price": 10},]}
 
-    Return:
+    Returns:
     < 200 OK $Invoice
     < 400 Bad Request
 
@@ -42,9 +42,19 @@ def invoice_create():
 
 
 @app.route('/api/client/{version}/invoices/<invoice_id>'.format(version=CURRENT_API_VERSION), methods=['GET'])
-def invoice_info(invoice_id):
+def invoice_get_info(invoice_id):
     """
     Get invoice info by invoice id.
-    """
 
-    pass
+    Returns:
+    < 200 OK $Invoice
+    < 404 Not Found
+    """
+    invoice = Invoice.query.get(invoice_id)
+    if not invoice:
+        raise NotFoundError()
+
+    schema = InvoiceSchema()
+
+    result = schema.dump(invoice)
+    return jsonify(result.data)
