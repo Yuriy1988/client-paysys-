d7d124b3468c71852dbb849163c1d450621b8a17
Serhii Kostel <serhii.kostel@digitaloutlooks.com>
2016-04-20 15:16:34 +0300

    Update notification api. Remove unused files.

--- periphery/notification_api.py
+++ periphery/notification_api.py
@@ -1,10 +1,19 @@
 from celery import Celery
 
-from api import app
-from periphery.service_error import handle_service_unavailable_error
+from api import app, errors
 
 
-@handle_service_unavailable_error(msg="Notification service is unavailable now.")
 def send_email(email, subject, message):
-    Celery(broker=app.config["NOTIFICATION_SERVER_URL"]).send_task('notify.send_mail', (email, subject, message))
+    try:
+        Celery(broker=app.config["NOTIFICATION_SERVER_URL"]).send_task('notify.send_mail', (email, subject, message))
+    except Exception as ex:
+        raise errors.ServiceUnavailable("Notification service is unavailable now.")
     return "Message sent to {email}".format(email=email)
+
+
+def notify(email, payment):
+    return send_email(
+        email,
+        "XOPAY transaction status",
+        "Thank you for your payment! Transaction status is: {status}".format(status=payment.status)
+    )
