21ff10fe275255ac5d66db9f6100d61668e7ebff
Daniel Omelchenko <daniel.omelchenko@digitaloutlooks.com>
2016-05-04 18:13:25 +0300

    Helper modification and tests

--- api/helper.py
+++ api/helper.py
@@ -1,3 +1,4 @@
+import re
 from collections import namedtuple
 from decimal import Decimal
 
@@ -16,6 +17,10 @@ def calculate_contract_tax(contract, total_price):
     """
     commission_fixed = Decimal(contract.get('commission_fixed', 0))
     commission_pct = Decimal(contract.get('commission_pct', 0))
+    print(str(contract.get("name")).ljust(10),
+          str(round(commission_fixed, 1)).ljust(10),
+          str(round((total_price * commission_pct) / Decimal(100), 1)).ljust(10),
+          )
     return commission_fixed + (total_price * commission_pct) / Decimal(100)
 
 
@@ -28,7 +33,14 @@ def _clean_up(contract):
     return {key: value for key, value in contract.items() if key not in {'contract_doc_url', 'filter', 'active'}}
 
 
-def get_route(paysys_id, merchant_id, total_price, currency):
+def get_filter(card_number):
+    def re_filter(contract):
+        filters = contract.get("filter", "*").replace(" ", "").replace("*", ".*").split(",")
+        return any(re.compile(f).search(card_number) for f in filters)
+    return re_filter
+
+
+def get_route(paysys_id, merchant_id, total_price, currency, card_number=""):
     """
     Gives the most profitable payment route as couple of contracts.
     Clean up contracts from excess fields.
@@ -37,8 +49,14 @@ def get_route(paysys_id, merchant_id, total_price, currency):
     :param str merchant_id: merchant identifier (payment destination)
     :param decimal total_price: Payment total price.
     :param str currency: Payment currency.
+    :param card_number: masked card_number for routing
     :return namedtuple: ($PaysysContract[Cleaned_up*], $MerchantContract[Cleaned_up*]) (see doc)
     """
+    print("==========================")
+    print("for amount:", total_price)
+    print("with card number:", card_number)
+    print("NAME       FIX        PCT")
+
     # get contracts from admin server via API
     paysys_contracts = services.get_payment_system_contracts(paysys_id, currency)   # -
     if not paysys_contracts:
@@ -48,12 +66,20 @@ def get_route(paysys_id, merchant_id, total_price, currency):
     if not merchant_contracts:
         raise errors.InternalServerError('Merchant contracts not found.')
 
+    # filtering
+    if card_number:
+        paysys_contracts = filter(get_filter(card_number), paysys_contracts)
+
     # calculate tax for every contracts
     paysys_with_tax = ((contract, calculate_contract_tax(contract, total_price)) for contract in paysys_contracts)
     merchant_with_tax = ((contract, calculate_contract_tax(contract, total_price)) for contract in merchant_contracts)
-
     # find most profitable deals (contracts)
+    print("----------pay-sys---------")
     ps_contract, ps_tax = min(paysys_with_tax, key=lambda v: v[1])
+    print('min:', ps_contract, ps_tax)
+    print("--------------------------")
+    print("---------merchant---------")
     mr_contract, mr_tax = max(merchant_with_tax, key=lambda v: v[1])
+    print('max:', mr_contract, mr_tax)
 
     return Route(paysys_contract=_clean_up(ps_contract), merchant_contract=_clean_up(mr_contract))
--- api/tests/helper.py
+++ api/tests/helper.py
@@ -8,13 +8,14 @@ from api import services, helper
 # Util functions:
 
 
-def contract_factory(contract_id, commission_fixed, commission_pct):
+def contract_factory(contract_id, name, commission_fixed, commission_pct, filter="*"):
     return {
         "id": contract_id,
+        "name": name,
         "commission_fixed": commission_fixed,
         "commission_pct": commission_pct,
         "contract_doc_url": "",
-        "filter": "",
+        "filter": filter,
         "active": True
     }
 
@@ -31,18 +32,20 @@ class TestHelper(unittest.TestCase):
 
     def setUp(self):
         services.get_payment_system_contracts = MagicMock(
-            return_value= [
-                contract_factory(1, '3.0', '2.6'),
-                contract_factory(2, '4.0', '1.8'),
-                contract_factory(3, '5.0', '1.0'),  # min
-                contract_factory(4, '0.0', '3.0'),  # max
-                contract_factory(5, '0.0', '2.8'),
+            return_value=[
+                contract_factory(1, 'visa',       '7.0', '2.5', "4*"),
+                contract_factory(2, 'master',     '8.0', '2.3', "5*"),
+                contract_factory(3, 'privat all', '0.0', '2.4', "*"),
+                contract_factory(4, 'privat',     '0.0', '1.9', "521857*, 516798*"),
+                contract_factory(5, 'aval',       '0.0', '2.0', "414951*"),
+                contract_factory(6, 'aval all',   '0.0', '2.6', "*"),
+                contract_factory(7, 'shved',      '10.0', '2.4', "*"),
             ])
         services.get_merchant_contracts = MagicMock(
-            return_value= [
-                contract_factory(1, '0', '3.1'),
-                contract_factory(2, '0', '4.0'),  # max
-                contract_factory(3, '1', '2.8'),  # min
+            return_value=[
+                contract_factory(1, 'merch1', '0', '3.1'),
+                contract_factory(2, 'merch2', '0', '4.0'),  # max
+                contract_factory(3, 'merch3', '1', '2.8'),  # min
             ])
         self.helper_get_route = functools.partial(helper.get_route, paysys_id=0, merchant_id=0, currency='USD')
 
@@ -51,15 +54,25 @@ class TestHelper(unittest.TestCase):
 
     def test_zero_total_price(self):
         route = self.helper_get_route(total_price=0)
-        self.assertEqual(bank_id(route), 4, msg=self.BANK_ID_ERROR.format(route))
+        self.assertEqual(bank_id(route), 3, msg=self.BANK_ID_ERROR.format(route))
         self.assertEqual(merchant_id(route), 3, msg=self.MERCHANT_ID_ERROR.format(route))
 
     def test_100_total_price(self):
-        route = self.helper_get_route(total_price=100)
+        route = self.helper_get_route(total_price=100, card_number="414951*****123")
         self.assertEqual(bank_id(route), 5, msg=self.BANK_ID_ERROR.format(route))
         self.assertEqual(merchant_id(route), 2, msg=self.MERCHANT_ID_ERROR.format(route))
 
-    def test_1000_total_price(self):
-        route = self.helper_get_route(total_price=1000)
+    def test_1000_total_price_privat(self):
+        route = self.helper_get_route(total_price=1000, card_number="516798*****123")
+        self.assertEqual(bank_id(route), 4, msg=self.BANK_ID_ERROR.format(route))
+        self.assertEqual(merchant_id(route), 2, msg=self.MERCHANT_ID_ERROR.format(route))
+
+    def test_1000_total_privat_all(self):
+        route = self.helper_get_route(total_price=1000, card_number="500000*****123")
         self.assertEqual(bank_id(route), 3, msg=self.BANK_ID_ERROR.format(route))
+        self.assertEqual(merchant_id(route), 2, msg=self.MERCHANT_ID_ERROR.format(route))
+
+    def test_100000_master(self):
+        route = self.helper_get_route(total_price=1000000, card_number="500000*****123")
+        self.assertEqual(bank_id(route), 2, msg=self.BANK_ID_ERROR.format(route))
         self.assertEqual(merchant_id(route), 2, msg=self.MERCHANT_ID_ERROR.format(route))
