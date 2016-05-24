eabc801bc33395ade31a1f781dcde8f9407dd832
Serhii Kostel <serhii.kostel@digitaloutlooks.com>
2016-05-24 14:24:41 +0300

    Add test coverage

--- manage.py
+++ manage.py
@@ -1,36 +1,69 @@
 #!venv/bin/python
 import os
 import unittest
-import flask_script as script
-import flask_migrate as migrate
+import coverage
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+
+COV = coverage.coverage(
+    branch=True,
+    include='api/*',
+    omit=[
+        'api/__init__.py',
+        'api/tests/*',
+        'api/*/__init__.py'
+    ]
+)
+COV.start()
 
 from api import app, db
 
 __author__ = 'Kostel Serhii'
 
-manager = script.Manager(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+
+# db (migrations)
+manager.add_command('db', MigrateCommand)
+
 
 # runserver
-server = script.Server(host="0.0.0.0", port=7254, use_debugger=True)
+# TODO: move port and debug into config
+server = Server(host="0.0.0.0", port=7254, use_debugger=True)
 manager.add_command('runserver', server)
 
 
-# test
+# tests
+def _api_test():
+    tests_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'api/tests')
+    suite = unittest.TestLoader().discover(tests_path, pattern='*.py')
+    return unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+@manager.command
 def test():
-    base = os.path.dirname(os.path.realpath(__file__))
-
-    api_tests_path = os.path.join(base, 'api', 'tests')
-    api_suite = unittest.TestLoader().discover(api_tests_path, pattern='*.py')
-
-    unittest.TextTestRunner(verbosity=2).run(api_suite)
-
-test_command = script.Command(test)
-manager.add_command('test', test_command)
+    """Runs unit tests into api/tests path."""
+    result = _api_test()
+    return 0 if result.wasSuccessful() else 1
 
 
-# db migrations
-migration = migrate.Migrate(app, db)
-manager.add_command('db', migrate.MigrateCommand)
+@manager.command
+def test_cover():
+    """Runs the unit tests into api/tests path with coverage."""
+    result = _api_test()
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'test_cover')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
+        return 0
+    return 1
 
 
 if __name__ == "__main__":
